973e1789fb1ea66837de9bfb93c6632cb47b6dbf
Vlad Kovaliov <v.v.kovaliov@gmail.com>
2022-12-16 23:15:01 +0300

    fix: adds module declaration

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare module 'least-frequently-used' {
+    class LFUCache {
+        constructor(capacity: number);
+        get(key: number): number;
+        put(key: number, value: number): void;
+    }
+};
